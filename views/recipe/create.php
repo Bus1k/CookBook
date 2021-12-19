a2d83b6cfc638bb841db2fc74429fc28bb4e67ca
Bus1k <busiki@interia.pl>
2021-12-19 17:28:33 +0100

    modify create recipe styles

--- views/recipe/create.php
+++ views/recipe/create.php
@@ -1,31 +1,31 @@
-<div class='form'>
+<div class='form addRecipe'>
     <form action="/recipe/add" method="POST" enctype="multipart/form-data">
         <div class="container">
-
+            <h1>Create new recipe </h1>
 
             <div class='inputs'>
-                <label for="title"><b>Title</b></label>
-                <input type="text" placeholder="Enter name" name="title" id="title" class="">
+                <label for="name"><b>Name</b></label>
+                <input type="text" placeholder="" name="name" id="name" required>
+                <div class='validAlert'></div>
             </div>
-
             <div class='inputs'>
                 <label for="description"><b>Description</b></label>
-                <textarea rows="4" cols="50" placeholder="Enter Description" name="description" id="description" class=""></textarea>
+                <textarea name="description" id="description" cols="30" rows="4" required></textarea>
+                <div class='validAlert'></div>
             </div>
-
             <div class='inputs'>
                 <label for="ingredients"><b>Ingredients</b></label>
-                <textarea rows="4" cols="50" placeholder="Enter Ingredients" name="ingredients" id="ingredients" class=""></textarea>
+                <textarea name="ingredients" id="ingredients" cols="30" rows="3" required></textarea>
+                <div class='validAlert'></div>
             </div>
-
             <div class='inputs'>
-                <label for="prep_time"><b>Preparation time</b></label>
-                <input type="number" placeholder="Set minutes" name="prep_time" id="prep_time" class="">
+                <label for="time"><b>Cooking time</b></label>
+                <input type="number" name="time" id="time" required>
+                <div class='validAlert'></div>
             </div>
-
             <div class='inputs'>
-                <label for="level">Choose a level:</label>
-                <select name="level" id="level">
+                <label for="level"><b>Choose a level</b></label>
+                <select name="level" id="level" required>
                     <option value="">--Please choose an option--</option>
                     <option value="beginner">Beginner</option>
                     <option value="elementary">Elementary</option>
@@ -33,15 +33,11 @@
                     <option value="advanced">Advanced</option>
                 </select>
             </div>
-
             <div class='inputs'>
                 <label for="photo"><b>Add photo</b></label>
                 <input type="file" placeholder="Add photo" name="photo" id="photo" class="">
             </div>
-
-
-            <button type="submit" class="registerbtn">Register</button>
-            <p>Already have an account? <a href="/login">Sign in</a>.</p>
+            <button type="submit" class="registerbtn">Add new recipe</button>
         </div>
     </form>
 </div>
